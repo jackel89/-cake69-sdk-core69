8869f22be309719337e6c2465b597693f4ce63d0
jackel89 <jackel8900@gmail.com>
2021-09-06 23:27:40 -0700

    first commit

---
+++ dist/entities/weth9.d.ts
@@ -0,0 +1,7 @@
+import { Token } from './token';
+/**
+ * Known WBNB9 implementation addresses, used in our implementation of Ether#wrapped
+ */
+export declare const WBNB9: {
+    [chainId: number]: Token;
+};
